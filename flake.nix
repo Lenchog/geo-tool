3fe1c791aefa8960964a1038515d34fb9f665936
Lenny0939 <lennyescott@gmail.com>
2026-02-26 06:36:01 +1100

    init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-python,
+    }:
+    let
+      system = "x86_64-linux";
+
+      pythonVersion = "3.14";
+
+      pkgs = import nixpkgs { inherit system; };
+      myPython = nixpkgs-python.packages.${system}.${pythonVersion};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          myPython
+        ];
+        shellHook = ''
+          python --version
+        '';
+      };
+    };
+}
