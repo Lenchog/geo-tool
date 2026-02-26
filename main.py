e6c3f2eab24fd8ab606741b4bf8dd8e14e54cff1
Lenny0939 <lennyescott@gmail.com>
2026-02-26 11:26:01 +1100

    cleaned up code in various places, including adding comments

--- main.py
+++ main.py
@@ -2,76 +2,75 @@ import math
 
 # TODO figure out how to use multiple files. way too cluttered
 
+# asks for the shape, then calls other functions to figure out area and prints it
 def area():
     command = ''
     while command != 'q':
         # TODO reduce repeated code
         command = input("Which shape would you like to calculate the area of?\n")
         if 'circle' in command: 
-            print(f"It's {area_circle()} squared units!")
+            print(f"It's {areaCircle()} squared units!")
         elif 'square' in command or 'rectangle' in command:
-            print(f"It's {area_rectangle()} squared units!")
+            print(f"It's {areaRectangle()} squared units!")
         elif 'triangle' in command:
-            print(f"It's {area_triangle()} squared units!")
+            # we can imagine the triangle as simply half a rectangle
+            print(f"It's {1/2 * areaRectangle()} squared units!")
         # if the user wants to quit, it's not invalid. however, we don't need to handle the q case
         elif command != 'q':
             print("Invalid command, try again")
         
-def area_rectangle():
-    x = int(input("How wide is your shape?"))
-    y = int(input("How tall is your shape?"))
-    return x * y
+# takes the width and height to simply calculate the area of a theoretical rectangle
+# also useful with triangle calculations
+def areaRectangle():
+    width = int(input("How wide is your shape?"))
+    length = int(input("How tall is your shape?"))
+    return width * length
 
-def area_triangle():
-    # catches yes, Yes, Y, y
-    # TODO lowercase these 'y's
-    if 'y' in input("Do you know both the width and height of this triangle? Y/N\n"):
-        # we can utilise the rectangle algorithm
-        area = 1/2 * area_rectangle()
-    elif 'y' in input("Alright, do you know any angles? Y/N"):
-        # TODO I forgot the trig area formula, and I am not connected to the internet
-        todo()
-    else: print("Sorry, we can't calculate the area then")
-    return area
-
-def area_circle():
-    # TODO: maybe implement alg for circumference and diameter
-    radius = int(input("What is the radius?"))
-    # TODO copy and paste pi symbol
-    # PIr^2
+# use πr^2 to calculate the area of the circle
+def areaCircle():
+    radius = int(input("What is the radius?\n"))
     return math.pi * radius * radius
 
+# we take two sides and whether either is a hypotenuse,
+# then use pythagoras' theorem to calculate what's missing
 def pythogoras():
     side1 = int(input("Give me one side of the triangle\n"))
-    if "side" in input("Do you want to use another side, or an angle?\n")
-        side2 = int(input("Alright, how long is it?\n"))
-        if y in input("Are either of these the hypotenuse?"):
-            hypotenuse = largest(side1, side2)
-            known_side = smallest(side1, side2)
-            # a^2 + b^2 = c^2
-            # a = sqrt(c^2 - b^2)
-            unknown_side = math.sqrt(hypotenuse * hypotenuse - known_side * known_side))
-            print(f"The unknown side is {unknown_side} units long!")
-        else:
-            # a^2 + b^2 = c^2
-            # c = sqrt(a^2 + b^2)
-            hypotenuse = math.sqrt(side1 * side1 + side2 * side2)
-            print(f"The hypotenuse is {hypotenuse} units long!")
+    side2 = int(input("Alright, how long is it?\n"))
+    # are we trying to find the hypotenuse, or another side? if so, different formula
+    if y in input("Are either of these the hypotenuse?").lower():
+        hypotenuse = largest(side1, side2)
+        known_side = smallest(side1, side2)
+        # a^2 + b^2 = c^2
+        # a = sqrt(c^2 - b^2)
+        unknown_side = math.sqrt(hypotenuse * hypotenuse - known_side * known_side)
+        print(f"The unknown side is {unknown_side} units long!")
+    else:
+        # a^2 + b^2 = c^2
+        # c = sqrt(a^2 + b^2)
+        hypotenuse = math.sqrt(side1 * side1 + side2 * side2)
+        print(f"The unknown side is {unknown_side} units long!")
         
-def todo():
+
+def identifyTriangle():
     print("Not yet implemented!")
 
-print("Welcome to this mathematical program")
-command = ''
-# won't loop again if the user wants to quit 
-while command != 'q':
-    # TODO: figure out how to bold so we can do *A*rea to highlight A
-    command = input("What would you like to do?\nPress A for area, P for Pythogoras, T for triangle identification, or Q to quit whatever's open\n")
-    # TODO: find lowercase function
-    match command:
-        case 'a': area(),
-        case 'p': pythagoras(),
-        case 't': todo(),
-        case 'q': print('Goodbye!'),
-        case _: print("Invalid command")
+def main():
+    print("Welcome to this mathematical program")
+    command = ''
+    # won't loop again if the user wants to quit 
+    while command != 'q':
+        bold = "\033[1m"
+        end = "\033[0m"
+        # bolds characters.
+        # What would you like to do?
+        # Area | Pythagoras | Triangle Identification | Quit
+        command = input(f"What would you like to do?\n{bold}A{end}rea | {bold}P{end}ythogoras | {bold}T{end}riangle Identification | {bold}Q{end}uit\n").lower()
+        match command:
+            case 'a' | 'area': area(),
+            case 'p' | 'pythagoras': pythagoras(),
+            case 't' | 'triangle': identifyTriangle(),
+            case 'q' | 'quit': print('Goodbye!'),
+            case _: print("Invalid command")
 
+if __name__ == "__main__":
+    main()
