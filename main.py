a7a529eef95e8f87e848fe24a820b6f53ec31dfc
Lenchog <lennyescott@gmail.com>
2026-02-27 12:04:05 +1100

    removed unnecessary code

--- main.py
+++ main.py
@@ -5,23 +5,20 @@ from identifyTriangle import identifyTriangle
 
 def main():
     print("Welcome to this mathematical program")
-    command = ''
-    # won't loop again if the user wants to quit 
-    while command != 'q':
-        bold = "\033[1m"
-        end = "\033[0m"
-        # bolds characters.
-        # What would you like to do?
-        # Area | Pythagoras | Triangle Identification | Quit
-        command = input(f"What would you like to do?\n{bold}A{end}rea | {bold}P{end}ythogoras | {bold}T{end}riangle Identification | {bold}Q{end}uit\n").lower()
-        match command:
-            case 'a' | 'area': area(),
-            case 'p' | 'pythagoras': pythagoras(),
-            case 't' | 'triangle': identifyTriangle(),
-            case 'q' | 'quit':
-                print('Goodbye!'),
-                quit()
-            case _: print("Invalid command")
+    bold = "\033[1m"
+    end = "\033[0m"
+    # bolds characters.
+    # What would you like to do?
+    # Area | Pythagoras | Triangle Identification | Quit
+    command = input(f"What would you like to do?\n{bold}A{end}rea | {bold}P{end}ythogoras | {bold}T{end}riangle Identification | {bold}Q{end}uit\n").lower()
+    match command:
+        case 'a' | 'area': area(),
+        case 'p' | 'pythagoras': pythagoras(),
+        case 't' | 'triangle': identifyTriangle(),
+        case 'q' | 'quit':
+            print('Goodbye!'),
+            quit()
+        case _: print("Invalid command")
 
 if __name__ == "__main__":
     main()
