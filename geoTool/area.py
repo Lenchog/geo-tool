b91ed4c09ed11dd89a624dd522f26054773d6a3a
Lenchog <lennyescott@gmail.com>
2026-03-12 11:05:46 +1100

    moved print statement in area to one line

--- geoTool/area.py
+++ geoTool/area.py
@@ -3,27 +3,27 @@ import math
 
 # asks for the shape, then calls other functions to figure out area and prints it
 def area():
-    command = ''
-    while command != 'q':
+    while True:
         # for each we ask for the variables we need, and print the result
         command = input("Which shape would you like to calculate the area of?\n")
         if 'circle' in command: 
             radius = getInt("What is the radius?")
-            print(f"It's {areaCircle(radius)} squared units!")
+            area = areaCircle(radius)
         elif 'square' in command :
             width = getInt("Give a side length of the square")
-            print(f"It's {areaRectangle(width, width)} squared units!")
+            area = areaRectangle(width, width)
         elif 'triangle' in command:
             height = getInt("How tall is the triangle?")
             base = getInt("How wide is the base of the triangle?")
-            print(f"It's {areaTriangle(height, base)} squared units!")
+            area = areaTriangle(height, base)
         elif 'rectangle' in command:
             width = getInt("How wide is your rectangle?")
             length = getInt("How tall is your rectangle?")
-            print(f"It's {areaRectangle(width, length)} squared units!")
+            area = areaRectangle(width, length)
         # if the user wants to quit, it's not invalid. however, we don't need to handle the q case
-        elif command != 'q':
-            print("Invalid command, try again")
+        else:
+            break;
+        print(f"It's {area} squared units!")
         
 # 1/2 * base * height
 def areaTriangle(base, height):
