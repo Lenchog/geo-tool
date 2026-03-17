2d788ba52f2715b3b721a327ebf062f559af099d
Lenchog <lennyescott@gmail.com>
2026-03-17 15:55:48 +1100

    added comments and removed unnecessary lines in test to align with what we're supposed to do

--- test_sample.py
+++ test_sample.py
@@ -7,34 +7,37 @@ from geoTool.classifyTriangle import classifyTriangle
 
 class TestStringMethods(unittest.TestCase):
     def test_pythag(self):
+        # test finding the hypotenuse
         self.assertEqual(pythag_maths(3, 4, "y"), 5)
+        # test finding a different side
         self.assertEqual(pythag_maths(5, 4, "n"), 3)
 
     def test_circle_area(self):
+        # area of large circle
         self.assertEqual(areaCircle(5), 78.54)
+        # area of small circle
         self.assertEqual(areaCircle(1), 3.14)
 
     def test_triangle_area(self):
+        # area of large triangle
         self.assertEqual(areaTriangle(5, 4), 10)
+        # area of small triangle
         self.assertEqual(areaTriangle(1, 1), 1 / 2)
 
     def test_rectangle_area(self):
+        # area of large rectangle
         self.assertEqual(areaRectangle(5, 4), 20)
+        # area of small square
         self.assertEqual(areaRectangle(1, 1), 1)
-
-    def test_classify_triangles(self):
-        self.assertEqual(classifyTriangle([3, 4, 5]), "Right Angled")
-        self.assertEqual(classifyTriangle([3, 3, 3]), "Equilateral")
-        self.assertEqual(classifyTriangle([2, 3, 4]), "Scalene")
-        self.assertEqual(classifyTriangle([3, 3, 4]), "Isosceles")
-        self.assertEqual(classifyTriangle([1, 1, 2]), "Impossible")
 
     # every time an integer is inputted it runs through this.
     # therefore we don't need to test these cases for everything else
     def test_validate_int(self):
         with self.assertRaises(ValueError):
+            # test string
             validateInt("Hello World!")
         with self.assertRaises(Exception):
+            # test numbers smaller than or equal to 0
             validateInt(0)
             # gets converted to 0
             validateInt(0.9)
